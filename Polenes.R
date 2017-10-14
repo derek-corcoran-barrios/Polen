a735f66799db1393270a55e5b0394cd2adf37e01
derek-corcoran-barrios <gaiterodelsurdelmundo@gmail.com>
2017-10-14 16:00:01 -0300

    <First commit>

---
+++ Polenes.R
@@ -0,0 +1,62 @@
+library(rvest)
+library(lubridate)
+Paginas <- c(100:808)
+Paginas <- Paginas[-59]
+Paginas <- Paginas[-147]
+Paginas <- Paginas[-170]
+
+
+Fechas <- list()
+Niveles <- list()
+
+for (i in 1:length(Paginas)){
+  Polen <- read_html(paste0("http://www.polenes.cl/sitio/santiago.asp?idgraph=", Paginas[i]))
+  Fecha <-Polen %>% html_nodes("span") %>% html_text()
+  Fechas[[i]] <- sub(".*Periodo: *(.*?) *hasta*", "\\1", Fecha)
+  Nivel <- Polen %>% html_nodes("td") %>% html_text()  
+  Niveles[[i]] <- as.numeric(gsub("[^0-9]", "", Nivel[11:13], ""))
+  print(i)
+}
+
+
+Fechas <- do.call(rbind, Fechas)
+Fechas <- as.data.frame(Fechas)
+colnames(Fechas) <- c("Fechas")
+
+Niveles <- as.data.frame(do.call(rbind, Niveles))
+colnames(Niveles) <- c("arboles_total", "platano_oriental", "pastos")
+
+Alergia <- cbind(Fechas, Niveles)
+Alergia$Fechas <- as.character(Alergia$Fechas)
+Alergia$Fechas <-str_match(Alergia$Fechas, "Período:(.*?) hasta")[,2]
+Alergia$Fechas <- str_replace(Alergia$Fechas, "Enero de ", "1-")
+Alergia$Fechas <- str_replace(Alergia$Fechas, "Febrero de ", "2-")
+Alergia$Fechas <- str_replace(Alergia$Fechas, "Marzo de ", "3-")
+Alergia$Fechas <- str_replace(Alergia$Fechas, "Abril de ", "4-")
+Alergia$Fechas <- str_replace(Alergia$Fechas, "Mayo de ", "5-")
+Alergia$Fechas <- str_replace(Alergia$Fechas, "Junio de ", "6-")
+Alergia$Fechas <- str_replace(Alergia$Fechas, "Julio de ", "7-")
+Alergia$Fechas <- str_replace(Alergia$Fechas, "Agosto de ", "8-")
+Alergia$Fechas <- str_replace(Alergia$Fechas, "Septiembre de ", "9-")
+Alergia$Fechas <- str_replace(Alergia$Fechas, "Octubre de ", "10-")
+Alergia$Fechas <- str_replace(Alergia$Fechas, "Noviembre de ", "11-")
+Alergia$Fechas <- str_replace(Alergia$Fechas, "Diciembre de ", "12-")
+
+
+Alergia$Fechas <- dmy(Alergia$Fechas)
+
+Alergia$Anno <- year(Alergia$Fechas)
+Alergia$Mes <- month(Alergia$Fechas)
+Alergia$Semana <- week(Alergia$Fechas)
+Alergia <- dplyr::filter(Alergia, Anno > 2000)
+
+saveRDS(Alergia, "Alergia.rds")
+
+
+library(ggplot2)
+
+ggplot(Alergia, aes(x = Fechas, y = platano_oriental)) + geom_area()
+
+ggplot(Alergia, aes(x = Semana, y = platano_oriental)) + geom_point()
+
+
