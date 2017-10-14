9ca0ad72273de6e139d73e645cb0eb16393e9273
derek-corcoran-barrios <gaiterodelsurdelmundo@gmail.com>
2017-10-14 18:28:45 -0300

    <Todos los datos>

--- Polenes.R
+++ Polenes.R
@@ -21,7 +21,6 @@ for (i in 1:length(Paginas)){
   print(i)
 }
 
-
 Fechas <- do.call(rbind, Fechas)
 Fechas <- as.data.frame(Fechas)
 colnames(Fechas) <- c("Fechas")
@@ -30,6 +29,9 @@ Niveles <- as.data.frame(do.call(rbind, Niveles))
 colnames(Niveles) <- c("arboles_total", "platano_oriental", "pastos")
 
 Alergia <- cbind(Fechas, Niveles)
+library(stringr)
+
+
 Alergia$Fechas <- as.character(Alergia$Fechas)
 Alergia$Fechas <-str_match(Alergia$Fechas, "Período:(.*?) hasta")[,2]
 Alergia$Fechas <- str_replace(Alergia$Fechas, "Enero de ", "1-")
@@ -53,11 +55,80 @@ Alergia$Mes <- month(Alergia$Fechas)
 Alergia$Semana <- week(Alergia$Fechas)
 Alergia <- dplyr::filter(Alergia, Anno > 2000)
 
+
+Paginas <- c(8:99)
+Paginas <- Paginas[-4]
+Paginas <- Paginas[-10]
+Paginas <- Paginas[-20]
+Paginas <- Paginas[-39]
+Paginas <- Paginas[-49]
+Paginas <- Paginas[-49]
+Paginas <- Paginas[-49]
+Paginas <- Paginas[-49]
+Paginas <- Paginas[-52]
+Paginas <- Paginas[-52]
+Paginas <- Paginas[-52]
+Paginas <- Paginas[-54]
+Paginas <- Paginas[-66]
+Paginas <- Paginas[-66]
+Paginas <- Paginas[-78]
+
+
+Fechas <- list()
+Niveles <- list()
+
+for (i in 1:length(Paginas)){
+  Polen <- read_html(paste0("http://www.polenes.cl/sitio/santiago.asp?idgraph=", Paginas[i]))
+  Fecha <-Polen %>% html_nodes("span") %>% html_text()
+  Fechas[[i]] <- sub(".*Periodo: *(.*?) *hasta*", "\\1", Fecha)
+  Nivel <- Polen %>% html_nodes("td") %>% html_text()  
+  Niveles[[i]] <- as.numeric(gsub("[^0-9]", "", Nivel[11:13], ""))
+  print(i)
+}
+
+Fechas <- do.call(rbind, Fechas)
+Fechas <- as.data.frame(Fechas)
+colnames(Fechas) <- c("Fechas")
+
+Niveles <- as.data.frame(do.call(rbind, Niveles))
+colnames(Niveles) <- c("arboles_total", "platano_oriental", "pastos")
+
+Alergia2 <- cbind(Fechas, Niveles)
+library(stringr)
+
+
+Alergia2$Fechas <- as.character(Alergia2$Fechas)
+Alergia2$Fechas <-str_match(Alergia2$Fechas, "Período:(.*?) hasta")[,2]
+Alergia2$Fechas <- str_replace(Alergia2$Fechas, "Enero de ", "1-")
+Alergia2$Fechas <- str_replace(Alergia2$Fechas, "Febrero de ", "2-")
+Alergia2$Fechas <- str_replace(Alergia2$Fechas, "Marzo de ", "3-")
+Alergia2$Fechas <- str_replace(Alergia2$Fechas, "Abril de ", "4-")
+Alergia2$Fechas <- str_replace(Alergia2$Fechas, "Mayo de ", "5-")
+Alergia2$Fechas <- str_replace(Alergia2$Fechas, "Junio de ", "6-")
+Alergia2$Fechas <- str_replace(Alergia2$Fechas, "Julio de ", "7-")
+Alergia2$Fechas <- str_replace(Alergia2$Fechas, "Agosto de ", "8-")
+Alergia2$Fechas <- str_replace(Alergia2$Fechas, "Septiembre de ", "9-")
+Alergia2$Fechas <- str_replace(Alergia2$Fechas, "Octubre de ", "10-")
+Alergia2$Fechas <- str_replace(Alergia2$Fechas, "Noviembre de ", "11-")
+Alergia2$Fechas <- str_replace(Alergia2$Fechas, "Diciembre de ", "12-")
+
+
+Alergia2$Fechas <- dmy(Alergia2$Fechas)
+
+Alergia2$Anno <- year(Alergia2$Fechas)
+Alergia2$Mes <- month(Alergia2$Fechas)
+Alergia2$Semana <- week(Alergia2$Fechas)
+Alergia2 <- dplyr::filter(Alergia2, Anno > 2000)
+
+Alergia <- rbind(Alergia2, Alergia)
+
 saveRDS(Alergia, "Alergia.rds")
+
 
 
 library(ggplot2)
 library(dplyr)
+library(tidyr)
 
 ggplot(Alergia, aes(x = Fechas, y = platano_oriental)) + geom_area() + theme_classic()
 
@@ -65,6 +136,10 @@ ggplot(Alergia, aes(x = Fechas, y = platano_oriental)) + geom_area() + theme_cla
 Weekly <- Alergia %>% select(Semana, platano_oriental) %>%group_by(Semana) %>% summarise_all(funs(mean, sd, max, min))
 
 
-ggplot(Weekly, aes(x = Semana, y = mean))+  geom_ribbon(aes(ymax = max, ymin = min, fill = "red")) + geom_ribbon(aes(ymax = mean + sd, ymin = mean - sd, fill = "blue"), alpha = 1) + geom_line() + scale_fill_manual(name = "leyenda", values = c("blue", "red"), labels = c('Error estándar','Extremos')) + ylab("polen de platano oriental /m³ de aire") + theme(legend.position="bottom") + theme_classic()
+ggplot(Weekly, aes(x = Semana, y = mean))+  geom_ribbon(aes(ymax = max, ymin = min, fill = "red")) + geom_ribbon(aes(ymax = mean + sd, ymin = mean - sd, fill = "blue"), alpha = 1) + geom_line() + scale_fill_manual(name = "leyenda", values = c("blue", "red"), labels = c('Error estándar','Extremos')) + ylab("polen de platano oriental /m³ de aire") + theme_classic()  + theme(legend.position="bottom") + scale_x_continuous(breaks=seq(from = 2.5, to = 49.5, by = 4), labels = c("Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"))
+
+Weekly2 <- Alergia %>% select(Semana, platano_oriental, arboles_total, pastos) %>% gather(key = Especie, value = Polen, -Semana) %>%group_by(Semana, Especie) %>% summarise_if(is.numeric, funs(mean, sd, max, min))
+
+ggplot(Weekly2, aes(x = Semana, y = mean))+  geom_ribbon(aes(ymax = mean + sd, ymin = mean - sd, fill = Especie), alpha = 1) + geom_line(aes(lty = Especie)) + ylab("polen/m³ de aire") + theme_classic()  + theme(legend.position="bottom") + scale_x_continuous(breaks=seq(from = 2.5, to = 49.5, by = 4), labels = c("Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"))
 
 
